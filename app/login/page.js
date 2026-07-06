80b0369315a48347e24b415a18f1be5bce12a152
masum1234-gok <mirmohammadmasumbillah@gmail.com>
2026-07-06 20:22:32 +0600

    update

--- app/login/page.js
+++ app/login/page.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
     const [step, setStep] = useState(1);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(true);
     const [twoFACode, setTwoFACode] = useState("");
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
